e4bce81d28389df72200a85403b1c1793d619dae
Aritra Chakraborty <aritra.eureka@gmail.com>
2026-03-02 10:55:08 +0530

    state_variable_and_hooks

--- src/Components/Bodycomp.js
+++ src/Components/Bodycomp.js
@@ -1,12 +1,23 @@
 import Bodyitems from "./Bodyitems";
 import rawdata from "./Utils/mock_data";
+import { useState } from "react";
+
 
 //Body Element
 var Bodycomp=()=>{
+
+let[resdata,setResdata]=useState(rawdata);
+
     return(
         <div className="bodycomp">
+
+            <button className="Filter-btn" onClick={()=>{
+                debugger;
+                var filtereddata=rawdata.filter((data)=>data.card.card.info.avgRating>4.4);
+                setResdata(filtereddata);
+            }}>Filter</button>
             <div className="restraunt-block">
-                {rawdata.map(a=>(
+                {resdata.map(a=>(
                     <Bodyitems key={a.card.card.info.id} res={a.card.card}/>
                 ))}
                 
